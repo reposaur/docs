24e35cd7bd9680185d98e59a79ead0afb87e1384
João Cerqueira <joao@cerqueira.io>
2022-05-09 22:15:54 +0100

    build: fix issues with deps

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
+import Prism from 'prism-react-renderer/prism'
 import 'nextra-theme-docs/style.css'
+
+(typeof global !== "undefined" ? global : window).Prism = Prism
+
+require("prismjs/components/prism-rego")
 
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
