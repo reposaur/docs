949ff1e13380d9230d1cfd3e1ec053b58ad865e9
João Cerqueira <joao@cerqueira.io>
2022-05-10 15:40:53 +0100

    feat: add webvitals and ganalytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,5 +6,44 @@ import 'nextra-theme-docs/style.css'
 require('prismjs/components/prism-rego')
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() { dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export function reportWebVitals(metric) {
+  const url = process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT
+
+  if (!url) {
+    return
+  }
+
+  const body = JSON.stringify({
+    route: window.__NEXT_DATA__.page,
+    ...metric,
+  })
+
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body)
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true })
+  }
 }
