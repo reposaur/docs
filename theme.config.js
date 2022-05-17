73744840fa29ebd74e2a51b07036038a0649d08b
João Cerqueira <joao@cerqueira.io>
2022-05-17 12:41:44 +0100

    feat: added new logo

--- theme.config.js
+++ theme.config.js
@@ -4,6 +4,7 @@ export default {
   titleSuffix: ' – Reposaur',
   logo: (
     <>
+      <img src="/logo.png" className="mr-2" style={{ height: '58px' }} />
       <span className="mr-2 font-extrabold hidden md:inline">Reposaur</span>
       <span className="text-gray-600 font-normal hidden md:inline">
         Security & Compliance for GitHub
@@ -16,25 +17,34 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Reposaur: Security & Compliance for GitHub" />
-      <meta name="og:description" content="Reposaur: Security & Compliance for GitHub" />
+      <meta
+        name="description"
+        content="Reposaur: Security & Compliance for GitHub"
+      />
+      <meta
+        name="og:description"
+        content="Reposaur: Security & Compliance for GitHub"
+      />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://docs.reposaur.com/og.png" />
       <meta name="twitter:site:domain" content="docs.reposaur.com" />
       <meta name="twitter:url" content="https://docs.reposaur.com" />
-      <meta name="og:title" content="Reposaur: Security & Compliance for GitHub" />
-      <meta name="og:image" content="https://docs.reposaur.com/og.png" />
+      <meta
+        name="og:title"
+        content="Reposaur: Security & Compliance for GitHub"
+      />
+      {/*<meta name="og:image" content="https://docs.reposaur.com/og.png" />*/}
       <meta name="apple-mobile-web-app-title" content="Reposaur" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-icon-180x180.png"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/android-icon-192x192.png"
+        href="/android-chrome-192x192.png"
       />
       <link
         rel="icon"
@@ -42,22 +52,24 @@ export default {
         sizes="32x32"
         href="/favicon-32x32.png"
       />
+      {/*
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
         href="/favicon-96x96.png"
       />
+      */}
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      {/*<meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />*/}
     </>
   ),
   floatTOC: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Reposaur.</>
+  footerText: <>MIT {new Date().getFullYear()} © Reposaur.</>,
 }
